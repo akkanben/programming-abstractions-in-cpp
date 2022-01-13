e60445d0255a4f45ead14c3c2b2439525b283341
Ben Mills <fuzzyllama@gmail.com>
2022-01-12 17:05:13 -0800

    Adds exercise 12-13

---
+++ 12-dynamic-memory-management/12-rational-unit-test/Rational.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <string>
+
+#include "../../StanfordCPPLib/include/error.h"
+#include "../../StanfordCPPLib/include/strlib.h"
+#include "Rational.h"
+
+int gcd(int x, int y);
+
+Rational::Rational() {
+  num = 0;
+  den = 1;
+}
+
+Rational::Rational(int n) {
+  num = n;
+  den = 1;
+}
+
+Rational::Rational(int x, int y) {
+  if (y == 0) {
+    error("Rational: Division by zero");
+  }
+  if (x == 0) {
+    num = 0;
+    den = 1;
+  } else {
+    int g = gcd(abs(x), abs(y));
+    num = x / g;
+    den = abs(y) / g;
+    if (y < 0) {
+      num = -num;
+    }
+  }
+}
+
+std::string Rational::toString() {
+  bool neg = false;
+  std::string output;
+  if (num < 0) {
+    neg = true;
+    num = -num;
+  }
+  if (den == 1) {
+    output = integerToString(num);
+  } else if (num == den) {
+    output = "1";
+  } else if (num > den) {
+    Rational leftover = Rational(num % den, den);
+    output = integerToString(abs(num / den)) + " " + leftover.toString();
+  } else {
+    output = integerToString(num) + "/" + integerToString(den);
+  }
+  return neg ? "-" + output : output;
+}
+
+std::ostream &operator<<(std::ostream &os, Rational r) {
+  return os << r.toString();
+}
+
+Rational operator+(Rational r1, Rational r2) {
+  return Rational(r1.num * r2.den + r2.num * r1.den, r1.den * r2.den);
+}
+
+Rational operator-(Rational r1, Rational r2) {
+  return Rational(r1.num * r2.den - r2.num * r1.den, r1.den * r2.den);
+}
+
+Rational operator*(Rational r1, Rational r2) {
+  return Rational(r1.num * r2.num, r1.den * r2.den);
+}
+
+Rational operator/(Rational r1, Rational r2) {
+  return Rational(r1.num * r2.den, r1.den * r2.num);
+}
+
+bool operator==(Rational r1, Rational r2) {
+  if ((r1.num == r2.num) && (r1.den == r2.den)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool operator!=(Rational r1, Rational r2) { return !(r1 == r2); }
+
+bool operator<(Rational r1, Rational r2) {
+  double a = (double)r1.num / (double)r1.den;
+  double b = (double)r2.num / (double)r2.den;
+  if (a < b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool operator<=(Rational r1, Rational r2) {
+  double a = (double)r1.num / (double)r1.den;
+  double b = (double)r2.num / (double)r2.den;
+  if (a <= b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool operator>(Rational r1, Rational r2) {
+  double a = (double)r1.num / (double)r1.den;
+  double b = (double)r2.num / (double)r2.den;
+  if (a > b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool operator>=(Rational r1, Rational r2) {
+  double a = (double)r1.num / (double)r1.den;
+  double b = (double)r2.num / (double)r2.den;
+  if (a >= b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+Rational operator+=(Rational &r1, Rational r2) {
+  r1 = r1 + r2;
+  return r1;
+}
+
+Rational operator-=(Rational &r1, Rational r2) {
+  r1 = r1 - r2;
+  return r1;
+}
+
+Rational operator*=(Rational &r1, Rational r2) {
+  r1 = r1 * r2;
+  return r1;
+}
+
+Rational operator/=(Rational &r1, Rational r2) {
+  r1 = r1 / r2;
+  return r1;
+}
+
+Rational operator++(Rational &r, int) {
+  Rational one(1, 1);
+  Rational old = r;
+  r = r + one;
+  return old;
+}
+
+Rational operator++(Rational &r) {
+  Rational one(1, 1);
+  r = r + one;
+  return r;
+}
+
+Rational operator--(Rational &r, int) {
+  Rational one(1, 1);
+  Rational old = r;
+  r = r - one;
+  return old;
+}
+
+Rational operator--(Rational &r) {
+  Rational one(1, 1);
+  r = r - one;
+  return r;
+}
+
+int gcd(int x, int y) {
+  int r = x % y;
+  while (r != 0) {
+    x = y;
+    y = r;
+    r = x % y;
+  }
+  return y;
+}
---
+++ 12-dynamic-memory-management/12-rational-unit-test/main.cpp
@@ -0,0 +1,44 @@
+/* Programming Abstractions in C++
+ *
+ * Exercise 12-11
+ *
+ * Write a unit test for the Rational class introduced in section 6.3. If you
+ * have implemented the extensions to the Rational class described in exercise 8
+ * from Chapter 6, you should include those extensions in your unit test as
+ * well.
+ *
+ * Solution 2021 Ben Mills
+ */
+
+#include "Rational.h"
+
+#include <cassert>
+#include <iostream>
+
+int main() {
+  Rational a(1, 4);
+  Rational b(1, 2);
+  assert(a + b == Rational(3, 4));
+  assert(a - b == Rational(-1, 4));
+  assert(a * b == Rational(1, 8));
+  assert(a / b == Rational(1, 2));
+  assert(a == Rational(1, 4));
+  assert(a != b);
+  assert(a != 0);
+  assert(b < a == false);
+  assert(a > b == false);
+  assert(a >= a == true);
+  assert(a >= b == false);
+  assert(a <= a == true);
+  assert(a <= b == true);
+  assert((a += a) == b);                           // a = 1/2; b = 1/2
+  assert((b -= Rational(1, 4)) == Rational(1, 4)); // a = 1/2; b = 1/4
+  assert((b *= a) == Rational(1, 8));              // a = 1/2; b = 1/8
+  assert((b /= a) == Rational(1, 4));              // a = 1/2; b = 1/4
+  assert(a++ == Rational(1, 2));                   // a = 1 1/2; b = 1/4
+  assert(++b == (Rational(1, 4) + 1));             // a = 1 1/2; b = 1 1/4
+  assert(b-- == Rational(5, 4));                   // a = 1 1/2; b = -1/4
+  assert(--a == Rational(1, 2));                   // a = 1/2; b = 1 1/4
+  std::cout << "Rational class unit test succeeded." << std::endl;
+  return 0;
+}
